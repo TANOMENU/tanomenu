10656ba59715f049f1f244071230afb77eb5fae6
Breno Xavier <brenoxavier1227@gmail.com>
2021-06-03 16:13:21 -0300

    Profile page and controller

--- src/main/java/tanomenu/controllers/ProfileController.java
+++ src/main/java/tanomenu/controllers/ProfileController.java
@@ -1,10 +1,12 @@
 package tanomenu.controllers;
 
+import net.minidev.json.JSONObject;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import tanomenu.repository.UserRepository;
+
+import java.util.Map;
 
 @Controller
 @RequestMapping("/profile")
@@ -18,5 +20,15 @@ public class ProfileController {
     @GetMapping
     public ModelAndView index() {
         return new ModelAndView("profile");
+    }
+
+    @PutMapping
+    public void updateProfile(@RequestBody Map<String, Object> requestBody) {
+        System.out.println(requestBody);
+    }
+
+    @PutMapping("/security")
+    public void updateSecurity(@RequestBody Map<String, Object> requestBody) {
+        System.out.println(requestBody);
     }
 }
