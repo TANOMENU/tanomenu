8aea0f2dc6c03b49405b9333026f56d6410d1669
jlucasrods <jlucasrods@gmail.com>
2021-06-03 03:10:50 -0300

    refactor repositories and models * make repositories smarter and less redundant * add data persistence

--- src/main/java/tanomenu/repository/ProductRepository.java
+++ src/main/java/tanomenu/repository/ProductRepository.java
@@ -3,44 +3,7 @@ package tanomenu.repository;
 import org.springframework.stereotype.Component;
 import tanomenu.models.Product;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
 @Component
-public class ProductRepository {
+public class ProductRepository extends Repository<Product> {
 
-    private final List<Product> products = Collections.synchronizedList(new ArrayList<>());
-
-    public Product save(Product product) {
-        products.add(product);
-        return product.toBuilder().build();
-    }
-
-    public Product update(Long id, Product product) {
-        var result = products.stream()
-                .filter(p -> p.getId().equals(id))
-                .findFirst();
-        return result.map(p -> {
-            p.update(product);
-            return p.toBuilder().build();
-        }).orElseThrow();
-    }
-
-    public Boolean delete(Long id) {
-        return products.removeIf(p -> p.getId().equals(id));
-    }
-
-    public List<Product> findAll() {
-        return products.stream().map(p -> p.toBuilder().build())
-                .collect(Collectors.toList());
-    }
-
-    public Optional<Product> findId(Long id) {
-        return products.stream()
-                .filter(p -> p.getId().equals(id))
-                .findFirst();
-    }
 }
