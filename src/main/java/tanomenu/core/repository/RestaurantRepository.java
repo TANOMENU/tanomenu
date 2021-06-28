6da28225389c47205e7edb993688abfea97bf583
José Lucas Rodrigues Silva <jlucasrods@gmail.com>
2021-06-27 23:25:02 -0300

    rebase

--- src/main/java/tanomenu/core/repository/RestaurantRepository.java
+++ src/main/java/tanomenu/core/repository/RestaurantRepository.java
@@ -7,6 +7,7 @@ import tanomenu.core.entity.Restaurant;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.*;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -26,17 +27,24 @@ public class RestaurantRepository extends Repository<Restaurant> {
     }
 
     public List<Restaurant> findByName(String name){
-        Predicate<Restaurant> isCompanyName = r -> containsIgnoreCase(r.getCompanyName(), name);
-        Predicate<Restaurant> isTradeName = r -> containsIgnoreCase(r.getTradeName(), name);
-        Predicate<Restaurant> isCategory = r -> containsIgnoreCase(r.getCategory().getValue(), name);
+        Predicate<String> contains = target -> target.toLowerCase().contains(name.toLowerCase());
+        Predicate<Restaurant> isCompanyName = r -> contains.test(r.getCompanyName());
+        Predicate<Restaurant> isTradeName = r -> contains.test(r.getTradeName());
+        Predicate<Restaurant> isCategory = r -> contains.test(r.getCategory().getValue());
         return getStream()
                 .filter(isCompanyName.or(isTradeName).or(isCategory))
                 .collect(Collectors.toList());
     }
 
-
-
-    private boolean containsIgnoreCase(String target, String term) {
-        return  target.toLowerCase().contains(term.toLowerCase());
+    public List<Restaurant> shuffle(int limit) {
+        return getStream()
+                .limit(limit * 2L)
+                .collect(Collectors.collectingAndThen(Collectors.toList(), collected -> {
+                    Collections.shuffle(collected);
+                    return collected.stream();
+                }))
+                .limit(limit)
+                .collect(Collectors.toList());
     }
+
 }
--- src/main/java/tanomenu/web/controllers/HomeController.java
+++ src/main/java/tanomenu/web/controllers/HomeController.java
@@ -4,27 +4,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import tanomenu.core.entity.Restaurant;
 import tanomenu.core.repository.RestaurantRepository;
-import tanomenu.core.repository.UserRepository;
 
-import java.util.LinkedHashMap;
 import java.util.stream.Collectors;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/home")
 public class HomeController {
 
-    private final UserRepository userRepository;
     private final RestaurantRepository restaurantRepository;
 
-    public HomeController(UserRepository userRepository, RestaurantRepository restaurantRepository) {
-        this.userRepository = userRepository;
+    public HomeController(RestaurantRepository restaurantRepository) {
         this.restaurantRepository = restaurantRepository;
     }
 
     @GetMapping
-    public String index(Model model) {
+    public String home(Model model) {
         var restaurants = restaurantRepository.findAll();
         var restaurantsByCategories = restaurants.stream()
                 .filter(r -> r.getCategory() != null)
